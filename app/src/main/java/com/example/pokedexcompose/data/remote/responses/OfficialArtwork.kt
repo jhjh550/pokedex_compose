86883d010d69549ce9ca5be2fcad723f0b3c6229
jhjh550 <jhjh550@gmail.com>
2021-08-14 21:28:24 +0900

    retrofit setup

---
+++ app/src/main/java/com/example/pokedexcompose/data/remote/responses/OfficialArtwork.kt
@@ -0,0 +1,5 @@
+package com.example.pokedexcompose.data.remote.responses
+
+data class OfficialArtwork(
+    val front_default: String
+)
---
+++ app/src/main/java/com/example/pokedexcompose/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.pokedexcompose.util
+
+object Constants {
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+}
