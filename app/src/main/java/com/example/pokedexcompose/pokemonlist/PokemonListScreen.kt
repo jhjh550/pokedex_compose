11192ebf5043c799565c661fb9116bbc7940fc0a
jhjh550 <jhjh550@gmail.com>
2021-08-18 21:50:03 +0900

    retry button

--- app/src/main/java/com/example/pokedexcompose/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokedexcompose/pokemonlist/PokemonListScreen.kt
@@ -1,7 +1,5 @@
 package com.example.pokedexcompose.pokemonlist
 
-import android.media.ImageReader
-import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,23 +8,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -35,8 +28,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
 import com.example.pokedexcompose.R
 import com.example.pokedexcompose.data.models.PokedexListEntry
 import com.example.pokedexcompose.ui.theme.RobotoCondensed
@@ -138,6 +129,20 @@ fun PokemonList(
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
         }
     }
+
+    Box(
+        contentAlignment = Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        if(isLoading){
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+        if(loadError.isNotEmpty()){
+            RetrySection(error = loadError) {
+                viewModel.loadPokemonPaginated()
+            }
+        }
+    }
 }
 
 
@@ -222,3 +227,31 @@ fun PokedexRow(
         Spacer(modifier = Modifier.height(16.dp))
     }
 }
+
+@Composable
+fun RetrySection(
+    error: String,
+    onRetry: () -> Unit
+){
+    Column {
+        Text(error, color = Color.Red, fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = { onRetry() },
+            modifier = Modifier.align(CenterHorizontally)
+        ) {
+            Text("Retry")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
